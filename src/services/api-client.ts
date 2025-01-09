b585df5aa3e750c0bc287ae2ff9d4ba6034596c2
Amirreza Moghtader <amirreza.moghtader@yahoo.ca>
2025-01-09 04:59:47 -0600

    created custom hook for game fetch

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '4577c9dea92f4a6c8d22b23ce44809c4'
+        key: '3d51b987c6a24e3d9b32cc0e3242bffd'
     }
 })
